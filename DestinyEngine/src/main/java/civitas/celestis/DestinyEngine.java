99ee944c2a100e0a2de99738d89c7afec4569a14
themrsung <themrsung@gmail.com>
2023-07-29 05:05:50 +0900

    Main class

--- DestinyEngine/src/main/java/civitas/celestis/DestinyEngine.java
+++ DestinyEngine/src/main/java/civitas/celestis/DestinyEngine.java
@@ -1,6 +1,10 @@
 package civitas.celestis;
 
-public class Main {
+/**
+ * <h2>DestinyEngine</h2>
+ * <p>The main class of Destiny.</p>
+ */
+public final class DestinyEngine {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
