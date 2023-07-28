6cb04330e3c9e8d34b46ac0b14aa61125e80c61d
themrsung <themrsung@gmail.com>
2023-07-29 05:09:36 +0900

    Module info

---
+++ DestinyEngine/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * <h2>DestinyEngine</h2>
+ * <p>
+ *     The main module of Destiny.
+ *     This module only contains the core operation code.
+ *     All utility and type classes can be found in the library modules.
+ * </p>
+ */
+module DestinyEngine {
+}
---
+++ DestinyLibs/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * <h2>DestinyLibs</h2>
+ * <p>
+ *     This module contains all types used in destiny.
+ * </p>
+ */
+module DestinyLibs {
+}
